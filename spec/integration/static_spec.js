a3110ae6bd159fa8f5a0b23b51bc187e14ef47d3
Cesar Jimenez <cijgr@hotmail.com>
2019-01-28 13:14:16 -0600

    Assignment 2 intro to express: completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -2,18 +2,33 @@ const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000";
 
+const routeMarco = `${base}/marco`;
+
 describe("routes : static", () => {
 
    describe("GET /", () => {
 
-      it("should return status code 200", (/*done*/) => { // This test throws a NOT DEFINED error with parameter 'done' and function 'done()'
+      it("should return status code 200", (done) => {
 
          request.get(base, (err, res, body) => {
             expect(res.statusCode).toBe(200);
-            expect(body).toContain("Welcome to Bloccit");
-            //done();
+            done();
          });
       });
+   });
+
+
+   describe("GET /marco", () => {
+
+      it("should return status code 200 along with the body: 'polo' ", (done) => {
+
+         request.get(routeMarco, (err, res, body) => {
+            expect(res.statusCode).toBe(200);
+            expect(res.body).toContain('polo');
+            done();
+         })
+      })
+
    });
 
 });
