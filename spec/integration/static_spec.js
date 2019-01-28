db55ae92777f03bb04b3bdc22ca723fdeb110ac7
Cesar Jimenez <cijgr@hotmail.com>
2019-01-28 12:35:32 -0600

    Checkpoint 1 intro to Express: completed

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,19 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000";
+
+describe("routes : static", () => {
+
+   describe("GET /", () => {
+
+      it("should return status code 200", (/*done*/) => { // This test throws a NOT DEFINED error with parameter 'done' and function 'done()'
+
+         request.get(base, (err, res, body) => {
+            expect(res.statusCode).toBe(200);
+            expect(body).toContain("Welcome to Bloccit");
+            //done();
+         });
+      });
+   });
+
+});
