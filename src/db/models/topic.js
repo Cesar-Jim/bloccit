405b577e7c9f0d2a232b4f896c5466f56e097553
Cesar Jimenez <cijgr@hotmail.com>
2019-02-04 18:49:51 -0600

    Flair 1 successfully completed

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,28 +1,36 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  var Topic = sequelize.define('Topic', {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
+  var Topic = sequelize.define(
+    "Topic",
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
     },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  }, {});
-  Topic.associate = function (models) {
+    {}
+  );
+  Topic.associate = function(models) {
     // associations can be defined here
     Topic.hasMany(models.Banner, {
       foreignKey: "topicId",
-      as: "banners",
+      as: "banners"
     });
     Topic.hasMany(models.Rule, {
       foreignKey: "topicId",
-      as: "rules",
+      as: "rules"
     });
     Topic.hasMany(models.Post, {
       foreignKey: "topicId",
       as: "posts"
+    });
+    Topic.hasMany(models.Flair, {
+      foreignKey: "topicId",
+      as: "flairs"
     });
   };
   return Topic;
