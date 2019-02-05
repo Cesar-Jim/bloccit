bc320ba09f1fe64c29cf9640e2646ed28cffcccc
Cesar Jimenez <cijgr@hotmail.com>
2019-02-05 12:01:41 -0600

    Assignment 10 validation: complete

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -89,6 +89,29 @@ describe("routes : topics", () => {
             }
          );
       });
+
+      it("should not create a new topic that fails validations", (done) => {
+         const options = {
+            url: `${base}create`,
+            form: {
+               title: 'x',
+               description: 'y'
+            }
+         };
+         request.post(options,
+            (err, res, body) => {
+               Topic.findOne({ where: { title: "x" } })
+                  .then((topic) => {
+                     expect(topic).toBeNull();
+                     done();
+                  })
+                  .catch((err) => {
+                     console.log(err);
+                     done();
+                  });
+            }
+         );
+      })
    });
 
 
--- src/routes/topics.js
+++ src/routes/topics.js
@@ -2,8 +2,8 @@
 
 const express = require("express");
 const router = express.Router();
-
 const topicController = require("../controllers/topicController") // Import the controller
+const validation = require("./validation");
 
 // ...And configure all the routes:
 router.get("/topics", topicController.index);
@@ -11,8 +11,8 @@ router.get("/topics/new", topicController.new); // Create the "new" view and pop
 router.get("/topics/:id", topicController.show); // Create the ":id" view to show the requested topic
 router.get("/topics/:id/edit", topicController.edit);
 
-router.post("/topics/create", topicController.create); // Create the "create" view to show newly created topic
+router.post("/topics/create", validation.validateTopics, topicController.create); // Create the "create" view to show newly created topic
 router.post("/topics/:id/destroy", topicController.destroy); // Create the "destroy" view and see that the topic has been deleted
-router.post("/topics/:id/update", topicController.update);
+router.post("/topics/:id/update", validation.validateTopics, topicController.update);
 
 module.exports = router;
