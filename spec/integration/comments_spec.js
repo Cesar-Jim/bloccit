7df78ae9013e991c13cf55e79895b8e4f12c6373
Cesar Jimenez <cijgr@hotmail.com>
2019-02-10 15:31:32 -0600

    Assignment 13 comments: complete

--- spec/integration/comments_spec.js
+++ spec/integration/comments_spec.js
@@ -211,7 +211,79 @@ describe("routes : comments", () => {
 
     });
 
+    describe("POST /topics/:topicId/posts/:postId/comments/:id/destroy", () => {
+
+      it("should NOT delete the comment with the associated ID", (done) => {
+        Comment.all()
+          .then((comments) => {
+            const commentCountBeforeDelete = comments.length;
+
+            expect(commentCountBeforeDelete).toBe(1);
+
+            request.post(
+              `${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`,
+              (err, res, body) => {
+                expect(res.statusCode).toBe(302);
+                Comment.all()
+                  .then((comments) => {
+                    expect(err).toBeNull();
+                    expect(comments.length).toBe(commentCountBeforeDelete - 1);
+                    done();
+                  })
+
+              });
+          })
+
+      });
+    });
+
   });
   ////////////////////////// ENDS CONTEXT: SIGNED IN USERS //////////////////////////
 
+  ////////////////////////// CONTEXT: ADMIN USER //////////////////////////
+  describe("admin user performing CRUD actions for Comment", () => {
+
+    beforeEach((done) => {    // before each suite in this context
+      request.get({           // mock authentication
+        url: "http://localhost:3000/auth/fake",
+        form: {
+          role: "admin",     // mock authenticate as member user
+          userId: this.user.id
+        }
+      },
+        (err, res, body) => {
+          done();
+        }
+      );
+    });
+
+    describe("POST /topics/:topicId/posts/:postId/comments/:id/destroy", () => {
+
+      it("should delete the comment with the associated ID", (done) => {
+        Comment.all()
+          .then((comments) => {
+            const commentCountBeforeDelete = comments.length;
+
+            expect(commentCountBeforeDelete).toBe(1);
+
+            request.post(
+              `${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`,
+              (err, res, body) => {
+                expect(res.statusCode).toBe(302);
+                Comment.all()
+                  .then((comments) => {
+                    expect(err).toBeNull();
+                    expect(comments.length).toBe(commentCountBeforeDelete - 1);
+                    done();
+                  })
+              });
+          })
+      });
+    });
+  });
+  ////////////////////////// ENDS CONTEXT: ADMIN USER //////////////////////////
+
+
 });
+
+////////////////////////// ENDS TEST SUITE //////////////////////////
